237a429c8ef80281cd91e34df9f804f6a7955d90
Haren <harensdeveloper@gmail.com>
2018-05-17 19:20:42 +0100

    First letter capitalised

--- open.py
+++ open.py
@@ -2,7 +2,7 @@ import os
 app = command.split(' ', 1)[1]
 cmd2 = '{} {} {}'.format('open', '-a', app)
 if os.system(cmd2) == 0: # Tests to see if the application could be opened
-    pass
+    print(app.title(), 'opened')
 else:
     print('Could not open program')
 exec(open("main.py").read())
