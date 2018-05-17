39594d2095776c85712f72a850a54fcf7fd6b1f3
Haren <harensdeveloper@gmail.com>
2018-05-17 19:16:12 +0100

    Indentation Fix

--- open.py
+++ open.py
@@ -1,8 +1,8 @@
 import os
-        app = command.split(' ', 1)[1]
-        cmd2 = '{} {} {}'.format('open', '-a', app)
-        if os.system(cmd2) == 0: # Tests to see if the application could be opened
-            pass
-        else:
-            print('Could not open program')
-        exec(open("main.py").read())
+app = command.split(' ', 1)[1]
+cmd2 = '{} {} {}'.format('open', '-a', app)
+if os.system(cmd2) == 0: # Tests to see if the application could be opened
+    pass
+else:
+    print('Could not open program')
+exec(open("main.py").read())
