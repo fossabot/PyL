32b69fe695155f2824e22b49347d57cfa488daa2
Haren <harensdeveloper@gmail.com>
2018-05-18 13:44:13 +0100

    Launch option added

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ if platform.system() == 'Darwin': # MacOS only
     command = input('What do you want to do? ')
     command = command.lower() 
     cmd1 = command.split(' ', 1)[0] 
-    if cmd1 == 'open': # Tests if open is the first word in the command
+    if cmd1 == 'open' or cmd1 == 'launch': # Tests if 'open' or 'launch' is the first word in the command
         exec(open("open.py").read())
     elif command.lower() == 'exit':
         print('Thanks for visiting!')
